1680f8ec5eeffa056d328e63b5847fdb56e025ea
Cong Thanh <pdcthanh112@gmail.com>
2021-10-14 09:29:39 +0700

    add student

--- src/components/tutor-component/tutor-request/TutorRequest.jsx
+++ src/components/tutor-component/tutor-request/TutorRequest.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import "./style.css"
+import "./style.scss"
 
 
+const getListRequest = () => {
+    return fetch(`${process.env.REACT_APP_API_URL}/tutor-requests`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => {
+            return response.json();
+
+        })
+        .catch(err => console.log(err));
+}
 
 
 const Card = () => {
+    console.log("AAAAAAAAA" + getListRequest)
     return (
         <div className="col-11">
             <table border="1">
@@ -12,19 +27,22 @@ const Card = () => {
                     <th>No.</th>
                     <th>Content</th>
                     <th>Status</th>
+                    <th>Action</th>
                 </tr>
                 <tr>
                     <td>1</td>
                     <td>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum est sit amet urna suscipit, eu ornare arcu suscipit.
                             Morbi leo nunc, placerat non sem id, volutpat dignissim lectus. Cras hendrerit eget quam quis porttitor. Fusce ultricies ligula at congue ultricies.
-                            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum vel malesuada massa.
-                            Fusce tincidunt elit vel justo volutpat, sit amet feugiat ligula maximus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-                            ac turpis egestas. Sed in pretium magna. In in lectus tempus, ornare urna non, mollis risus. Vestibulum in tristique justo.
-                            <a href="#">view all</a>
-                            </p>
+
+                        </p>
+                        <a href="#">view all</a>
                     </td>
                     <td className="text">Pending</td>
+                    <td className="action">
+                        <button type="button" className="approve">Approve</button>
+                        <button type="button" className="reject">Reject</button>
+                    </td>
                 </tr>
 
             </table>
